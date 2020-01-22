03cc2d35ad1a0bf6d47f7a2ddc648225e392f33c
Utkarsh Mishra <utkarshmishra2022@u.northwestern.edu>
2020-01-22 01:07:34 -0600

    instock

--- src/App.js
+++ src/App.js
@@ -10,8 +10,25 @@ import { Drawer, List, ListItem, ListItemText, ListItemAvatar, Avatar } from '@m
 import CloseIcon from '@material-ui/icons/Close';
 import ToggleButton from '@material-ui/lab/ToggleButton';
 import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
-
 import SimpleMenu from './Menu'
+
+/*
+import firebase from 'firebase/app';
+import 'firebase/database';
+const firebaseConfig = {
+  apiKey: "AIzaSyB8CaRsqtVcKVbXXrFiGLsibyz_kLf1TBI",
+  authDomain: "shopping-cart-48a84.firebaseapp.com",
+  databaseURL: "https://shopping-cart-48a84.firebaseio.com",
+  projectId: "shopping-cart-48a84",
+  storageBucket: "shopping-cart-48a84.appspot.com",
+  messagingSenderId: "871412875135",
+  appId: "1:871412875135:web:6230cdf1003c976a94e44a",
+  measurementId: "G-1F5W6NRTNL"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
+*/
 
 const useStyles = makeStyles(theme => ({
   cardy: {
@@ -31,14 +48,22 @@ const useStyles = makeStyles(theme => ({
 const App = () => {
   const classes = useStyles();
   const [data, setData] = useState({});
+  const [inventory, setInventory] = useState({});
   const products = Object.values(data);
+
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
       const json = await response.json();
       setData(json);
     };
+    const fetchInventory = async () => {
+      const response1 = await fetch('./data/inventory.json');
+      const json1 = await response1.json();
+      setInventory(json1);
+    };
     fetchProducts();
+    fetchInventory();
   }, []);
 
   const [state, setState] = useState({ left: true });
@@ -53,54 +78,52 @@ const App = () => {
     setState({ ...state, [side]: open });
   };
   const [Items, setItems] = useState([]);
-  const arr = Object.values(Items)
-  const cartAdd = (shirt, boo) => {
+  const arr = Object.values(Items);
+  const cartAdd = (shirt, size, boo) => {
+    let sz;
+    let newInventory = inventory;
     if (boo) {
       let val = Items;
       let x = false;
       let i;
+      let j;
+          let val1 = bold;
       for (i = 0; i < Items.length; i++) {
         if (Items[i].value === shirt) {
-          x = true
-          break;
-        }
-      }
-      if (x) {
-        let j;
-        let val1 = bold;
-        for (j = 0; j < bold.length; j++) {
-          if (val1[j][0] === Items[i].value.title) {
-            if (Items[i].size === val1[j][1]) {
-              Items[i].quantity += 1;
-            }
-            else {
-              val.push({
-                value: shirt
-                , quantity: 1
-                , size: val1[j][1]
-              })
+          for (j = 0; j < bold.length; j++) {
+            if (val1[j][0] === Items[i].value.title) {
+              if (Items[i].size === val1[j][1]) {
+                x = true;
+                Items[i].quantity += 1;
+                sz = val1[j][1];
+                break;
+              }
+
             }
           }
+          if (x) { break; }
         }
-
       }
-      else {
+      
+      if(!x) {
         setBold(bold);
         let statement = false;
         let k;
-        for (k = 0; k < bold.length; k++) { 
-          if (bold[k][0] === shirt.title) {  
+
+        for (k = 0; k < bold.length; k++) {
+          if (bold[k][0] === shirt.title) {
             statement = true;
             break;
           }
         }
         if (statement) {
-          console.log('impossible')
           val.push({
             value: shirt
             , quantity: 1
             , size: bold[k][1]
           })
+
+          sz = bold[k][1];
         }
         else {
           val.push({
@@ -108,29 +131,37 @@ const App = () => {
             , quantity: 1
             , size: 'M'
           })
+          sz = 'M';
         }
 
       }
       setItems(val);
       setState({ right: true });
+      newInventory[shirt.sku][sz] -= 1;
+      setInventory(newInventory);
     }
     else {
-      cartRemove(shirt)
+      cartRemove(shirt, size)
       setState({ right: true });
+      setInventory(newInventory);
     }
-
   }
-
-  const cartRemove = (shirt) => {
+  
+  const cartRemove = (shirt, size) => {
     let val = Items;
+    let newInventory = inventory;
     let i;
+    let boo = false;
+    let sz;
     for (i = 0; i < Items.length; i++) {
-      if (Items[i].value === shirt) {
+      if (Items[i].value === shirt && Items[i].size === size) {
+        sz = size;
         Items[i].quantity -= 1;
+        newInventory[shirt.sku][sz] += 1;
         if (Items[i].quantity === 0) {
-          val.splice(val.indexOf(val[i]), 1)
+          val.splice(val.indexOf(val[i]), 1);
         }
-        break;
+        if(boo){break;}
       }
     }
     setItems(val);
@@ -149,6 +180,55 @@ const App = () => {
     const [alignment, setAlignment] = React.useState();
     const handleAlignment = (event, newAlignment) => {
       setAlignment(newAlignment);
+      ButtonExist(product);
+    };
+    let sizeSG;
+    let sizeMG;
+    let sizeLG;
+    let sizeXLG;
+
+    if (inventory[product.sku]) {
+      sizeSG = inventory[product.sku].S;
+      sizeMG = inventory[product.sku].M;
+      sizeLG = inventory[product.sku].L;
+      sizeXLG = inventory[product.sku].XL;
+    }
+    else {
+      sizeSG = true;
+      sizeMG = true;
+      sizeLG = true;
+      sizeXLG = true;
+    }
+
+    const [button, setButton] = useState({
+      small: sizeSG,
+      medium: sizeMG,
+      large: sizeLG,
+      xlarge: sizeXLG
+    });
+    const ButtonExist = () => {
+      let sizeS = false;
+      let sizeM = false;
+      let sizeL = false;
+      let sizeXL = false;
+      if (inventory[product.sku].S > 0) {
+        sizeS = true;
+      }
+      if (inventory[product.sku].M > 0) {
+        sizeM = true;
+      }
+      if (inventory[product.sku].L > 0) {
+        sizeL = true;
+      }
+      if (inventory[product.sku].XL > 0) {
+        sizeXL = true;
+      }
+      setButton({
+        small: sizeS,
+        medium: sizeM,
+        large: sizeL,
+        xlarge: sizeXL
+      });
     };
     return (
 
@@ -165,43 +245,41 @@ const App = () => {
             <Typography variant="body2" color="textSecondary" component="p">
               {"Price: $" + product.price}
             </Typography>
-
           </CardContent>
+
           <CardContent>
             <ToggleButtonGroup
               value={alignment}
               exclusive
               onChange={handleAlignment}
               aria-label="text alignment">
-              <ToggleButton value="left" onClick={() => handleBold(product, "S")} aria-label="left aligned">
+              {button.small && <ToggleButton value="left" onClick={() => handleBold(product, "S")} aria-label="left aligned">
                 S
-            </ToggleButton>
-              <ToggleButton value="center" onClick={() => handleBold(product, "M")} aria-label="centered">
+            </ToggleButton>}
+              {button.medium && <ToggleButton value="center" onClick={() => handleBold(product, "M")} aria-label="centered">
                 M
-            </ToggleButton>
-              <ToggleButton value="right" onClick={() => handleBold(product, "L")} aria-label="right aligned">
+            </ToggleButton>}
+              {button.large && <ToggleButton value="right" onClick={() => handleBold(product, "L")} aria-label="right aligned">
                 L
-            </ToggleButton>
-              <ToggleButton value="justify" onClick={() => handleBold(product, "XL")} aria-label="justified">
+            </ToggleButton>}
+              {button.xlarge && <ToggleButton value="justify" onClick={() => handleBold(product, "XL")} aria-label="justified">
                 XL
-            </ToggleButton>
+            </ToggleButton>}
             </ToggleButtonGroup>
           </CardContent>
           <CardContent>
-            <Button key={product.sku} onClick={() => cartAdd(product, true)} variant="contained" color="primary" style={{ paddingBottom: 10 }}>
+            <Button key={product.sku} onClick={() => cartAdd(product, 1, true)} variant="contained" color="primary" style={{ paddingBottom: 10 }}>
               Add to Cart
                 </Button>
           </CardContent>
-
-
         </Card>
       </Grid>
 
     )
 
   };
-  const [bold, setBold] = React.useState([]);
 
+  const [bold, setBold] = React.useState([]);
   const handleBold = (product, size) => {
     let val = bold;
     let statement = false;
@@ -218,7 +296,6 @@ const App = () => {
     setBold(val);
   };
   return (
-
     <div>
       <div>
         <Button onClick={toggleDrawer('right', true)} style={{ float: "right" }}><AddShoppingCartIcon style={{ fontSize: 50, float: "right" }} /></Button>
@@ -229,13 +306,13 @@ const App = () => {
           <div className={classes.list} role="presentation">
             <List>
               {arr.map((text) => (
-                <ListItem button key={text.value.title + text.value.size}>
+                <ListItem button key={text.value.sku + text.size}>
                   <ListItemAvatar>
                     <Avatar alt="Remy Sharp" src={"./data/products/" + text.value.sku + "_1.jpg"} />
                   </ListItemAvatar>
                   <ListItemText primary={text.value.title + ' | Size: ' + text.size + ' | Quantity: ' + text.quantity} />
                   <ListItemText style={{ marginRight: 50 }} primary={'$' + text.value.price} />
-                  <Button variant="contained" color="primary" onClick={() => cartAdd(text.value, false)}><CloseIcon size="large" /></Button>
+                  <Button variant="contained" color="primary" onClick={() => cartAdd(text.value, text.size, false)}><CloseIcon size="large" /></Button>
 
                 </ListItem>
               ))}
